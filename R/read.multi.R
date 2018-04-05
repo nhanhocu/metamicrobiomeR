910d0a077cd19e2215b70d125f1c7d7c6ec15ca3
Thi Ho <nth2111@neuro.columbia.edu>
2018-04-05 14:46:41 -0400

    update readme

--- R/read.multi.R
+++ R/read.multi.R
@@ -3,9 +3,10 @@
 #'
 #' This function reads multiple files of the same pattern in a directory into R.
 #' @param patht path to files.
-#' @param patternt pattern of files. Options are ".txt" or ".csv".
+#' @param patternt pattern of files. Options are ".txt" or ".csv" or ".sav" or ".sas7bdat".
 #' @param assignt assign files. Default is "no".
 #' @param study name of the study. Default is NULL.
+#' @param tolower.colnames turn all column names to lower case. Default is TRUE.
 #' @keywords multiple files
 #' @export
 #' @return list of all data files in the path
@@ -13,7 +14,7 @@
 #' patht<-system.file("extdata/QIIME_outputs/Bangladesh/alpha_div_collated", package = "metamicrobiomeR", mustWork = TRUE)
 #' alpha.ba<-read.multi(patht=patht,patternt=".txt",assignt="no",study="Bangladesh")
 
-read.multi<-function (patht,patternt=".txt",assignt="no",study=NULL){
+read.multi<-function (patht,patternt=".txt",assignt="no",study=NULL,tolower.colnames=TRUE){
   filenames <- paste(patht,list.files(path=patht, pattern=patternt),sep="/")
   if (patternt==".txt"){
     tmp <- lapply(filenames, function(x) read.delim(file=x))
@@ -21,10 +22,18 @@ read.multi<-function (patht,patternt=".txt",assignt="no",study=NULL){
   if (patternt==".csv"){
     tmp <- lapply(filenames, function(x) read.csv(file=x))
   }
+  if (patternt==".sav"){
+    tmp<-lapply(filenames,function(x) read.spss(file=x,use.value.labels=FALSE, to.data.frame=TRUE))
+  }
+  if (patternt==".sas7bdat"){
+    tmp<-lapply(filenames,function(x) read.sas7bdat(file=x, debug=FALSE))
+  }
   names(tmp)<-tolower(gsub(patternt,"", filenames))
   names(tmp)<-basename(tools::file_path_sans_ext(names(tmp)))
   for (i in 1:length(names(tmp))){
-    colnames(tmp[[i]])<-tolower(colnames(tmp[[i]]))
+    if (tolower.colnames==TRUE){
+      colnames(tmp[[i]])<-tolower(colnames(tmp[[i]]))
+    }
     tmp[[i]][,"study"]<-study
     if (assignt=="yes"){
       assign(names(tmp[i]),tmp[[names(tmp[i])]])
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-This is an initial version of the package metamicrobiomeR. To start, please download and view the steps/examples/workflow in the tutorial file "metamicrobiomeR_Supplementary_file". 
+This is an initial version of the package metamicrobiomeR.   
+
+To start, please download and view the steps/examples/workflow in the tutorial file "metamicrobiomeR_Supplementary_file". 
+
 The metamicrobiomeR package implements Generalized Additive Model for Location, Scale and Shape (GAMLSS) 
     with zero inflated beta (BEZI) family for analysis of microbiome relative abundance data and 
     random effect meta-analysis models for meta-analysis pooling estimates across microbiome studies.
@@ -6,3 +9,5 @@ The metamicrobiomeR package implements Generalized Additive Model for Location, 
     shared bacterial genera with the Bangladesh data (Subramanian et al 2014), 
     comparison of multiple diversity indexes using linear/linear mixed effect models 
     and some data display/visualization are also implemented.
+
+The paper describing the methods implemented in the "metamicrobiomeR" package as well as simulations, and example analyses/results are now available in preprint at: https://www.biorxiv.org/content/early/2018/04/04/294678. 
