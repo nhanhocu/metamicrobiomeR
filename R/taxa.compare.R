3ab29318c414691554545f1d070b0a7eee2d8963
nhanhocu <nhanhocumc@gmail.com>
2019-03-02 10:36:36 +0700

    Add normalization options

--- R/taxa.compare.R
+++ R/taxa.compare.R
@@ -3,7 +3,7 @@
 #' This function compares taxa relative abundance summary tables at all levels between groups using GAMLSS with BEZI or Linear/Linear Mixed Effect models (LM/LMEM) after filtering (using prevalence and relative abundance thresholds).
 #' @param taxtab taxa relative abundance table (already merged to mapping file) from phylum to species or any preferred highest taxa level.
 #' @param propmed.rel statistical method for comparing relative abundance. Options are "lm" for LM/LMEM or "gamlss" for GAMLSS with BEZI family.
-#' @param transform transformation of relative abundance data. Options are "none" for no transformation, "asin.sqrt" for arcsine transformation, "logit" for logit transformation, "clr" for centered log ratio transformation. Default is "none".
+#' @param transform transformation of relative abundance data. Options are "none" for no transformation, "gmpr" for Geometric Mean of Pairwise Ratios (GMPR) normalization, "asin.sqrt" for arcsine transformation, "logit" for logit transformation, "clr" for centered log ratio transformation. Default is "none".
 #' @param zeroreplace.method Method for zero replacement implemented in R package *zCompositions*. Options are "none" for no replacement, "multKM" for Multiplicative Kaplan-Meier smoothing spline (KMSS) replacement, "multLN" for Multiplicative lognormal replacement, "multRepl" for Multiplicative simple replacement, "lrEM" for Log-ratio EM algorithm, "lrDA" for Log-ratio DA algorithm. Default is "none".
 #' @param comvar main variable for comparison
 #' @param adjustvar variables to be adjusted.
@@ -28,7 +28,8 @@
 taxa.compare<-function(taxtab,propmed.rel="gamlss",transform="none",zeroreplace.method="none",
                        comvar,adjustvar,personid="personid",longitudinal="yes",
                        percent.filter=0.05,relabund.filter=0.00005,p.adjust.method="fdr",...){
-  #sapply(c("lme4", "gamlss","gdata","reshape2","plyr"), require, character.only = TRUE)
+  #sapply(c("lme4", "gamlss","gdata","reshape2","plyr","matrixStats","devtools","RCurl","httr","repmis","jsonlite"), require, character.only = TRUE)
+  require(gamlss)
   taxdat<-as.data.frame(taxtab)
   taxdat[,comvar]<-gdata::drop.levels(taxdat[,comvar],reorder=FALSE) #drop missing/unused level and keep level order
   if (longitudinal=="yes"){
@@ -63,6 +64,7 @@ taxa.compare<-function(taxtab,propmed.rel="gamlss",transform="none",zeroreplace.
     logittransform <- function(p) { log(p/(1-p)) }
     taxdat[,taxname]<-apply(taxdat[,taxname],2,logittransform )
   }
+  # Centered log ratio transformation
   if (propmed.rel=="lm" &transform=="clr"){
     #zero replacement using package zCompositions
     if (zeroreplace.method=="multLN"){
@@ -84,7 +86,74 @@ taxa.compare<-function(taxtab,propmed.rel="gamlss",transform="none",zeroreplace.
     clrdat<-as.data.frame(compositions::clr(test0))
     taxdat[,taxname]<-clrdat
   }
-  # begin models
+
+  #GMPR normalization
+  #source GMPR from github repo: "https://github.com/jchen1981/GMPR/blob/master/GMPR.R"
+  #url<-"https://github.com/jchen1981/GMPR/blob/master/GMPR.R"
+  #script <- getURL(url, ssl.verifypeer = FALSE)
+  #eval(parse(text = script))
+  #The functions to source R function directly from github sometimes does not work properly.
+  #The GMPR function from the above github repo is copied below for convenience.
+  require(matrixStats)
+  GMPR <- function (comm, intersect.no = 10, ct.min = 1, trace = TRUE) {
+    # mask counts < ct.min
+    comm[comm < ct.min] <- 0
+
+    if (is.null(colnames(comm))) {
+      colnames(comm) <- paste0('S', 1:ncol(comm))
+    }
+
+    if (trace) cat('Begin GMPR size factor calculation ...\n')
+
+    comm.no <- numeric(ncol(comm))
+    gmpr <- sapply(1:ncol(comm),  function(i) {
+      if (i %% 50 == 0) {
+        cat(i, '\n')
+      }
+      x <- comm[, i]
+      # Compute the pairwise ratio
+      pr <- x / comm
+      # Handling of the NA, NaN, Inf
+      pr[is.nan(pr) | !is.finite(pr) | pr == 0] <- NA
+      # Counting the number of non-NA, NaN, Inf
+      incl.no <- colSums(!is.na(pr))
+      # Calculate the median of PR
+      pr.median <- matrixStats::colMedians(pr, na.rm=TRUE)
+      # Record the number of samples used for calculating the GMPR
+      comm.no[i] <<- sum(incl.no >= intersect.no)
+      # Geometric mean of PR median
+      if (comm.no[i] > 1) {
+        return(exp(mean(log(pr.median[incl.no >= intersect.no]))))
+      } else {
+        return(NA)
+      }
+    }
+    )
+    if (sum(is.na(gmpr))) {
+      warning(paste0('The following samples\n ', paste(colnames(comm)[is.na(gmpr)], collapse='\n'),
+                     '\ndo not share at least ', intersect.no, ' common taxa with the rest samples! ',
+                     'For these samples, their size factors are set to be NA! \n',
+                     'You may consider removing these samples since they are potentially outliers or negative controls!\n',
+                     'You may also consider decreasing the minimum number of intersecting taxa and rerun the procedure!\n'))
+    }
+    if (trace) cat('Completed!\n')
+    if (trace) cat('Please watch for the samples with limited sharing with other samples based on NSS! They may be outliers! \n')
+    names(gmpr) <- names(comm.no) <- colnames(comm)
+    attr(gmpr, 'NSS') <- comm.no
+    return(gmpr)
+  }
+
+  #Begin GMPR normalization
+  if (transform=="gmpr"){
+    reltab<-t(taxdat[,taxname])
+    #calculate size factor
+    gmpr.sf<-GMPR(comm=reltab,intersect.no = 2, ct.min = 0, trace = TRUE)
+    #normalized by size factors
+    rel.gmpr<-t(t(reltab)/gmpr.sf)
+    taxdat[,taxname]<-rel.gmpr
+  }
+
+  # begin models for differential abundance testing
   estisum<-NULL
   for (i in 1: length(taxname)){
     print(i)
@@ -134,13 +203,17 @@ taxa.compare<-function(taxtab,propmed.rel="gamlss",transform="none",zeroreplace.
     #Generalized Additive Models for Location Scale and Shape (GAMLSS): Betazeroinflated (BEZI) family, mu link logit
     if (propmed.rel=="gamlss"){
       testdat<-taxdat[,c(taxname[i],comvar,adjustvar,"personid")]
-      testdat[,taxname[i]][testdat[,taxname[i]]==1]<-0.9999 # dirty fix for 1 value of relative abundance
+      testdat[,taxname[i]][testdat[,taxname[i]]>=1]<-0.9999 # dirty fix for 1 value of relative abundance
       testdat<-na.omit(testdat)
       if (longitudinal=="yes"){
-        fitsum<-try(summary(gamlss::gamlss(as.formula(paste(taxname[i],paste(c(comvar,adjustvar,"gamlss::random(personid)"),collapse="+"),sep="~")), gamlss.family = BEZI, data = testdat, trace = FALSE),save=TRUE))
+        fitsum<-try(summary(gamlss::gamlss(as.formula(paste(taxname[i],paste(c(comvar,adjustvar,"random(personid)"),collapse="+"),sep="~")), family = BEZI, data = testdat, trace = FALSE),save=TRUE))
+        #To check: gamlss:random(personid) will give different results from random(personid); gamlss.family=BEZI will give different results from family=BEZI
+        #fitsum<-try(summary(gamlss::gamlss(as.formula(paste(taxname[i],paste(c(comvar,adjustvar,"gamlss::random(personid)"),collapse="+"),sep="~")), gamlss.family = BEZI, data = testdat, trace = FALSE),save=TRUE))
       }
       if (longitudinal=="no"){
-        fitsum<-try(summary(gamlss::gamlss(as.formula(paste(taxname[i],paste(c(comvar,adjustvar),collapse="+"),sep="~")), gamlss.family = BEZI, data = testdat, trace = FALSE),save=TRUE))
+        fitsum<-try(summary(gamlss::gamlss(as.formula(paste(taxname[i],paste(c(comvar,adjustvar),collapse="+"),sep="~")), family = BEZI, data = testdat, trace = FALSE),save=TRUE))
+        #To check: gamlss:random(personid) will give different results from random(personid); gamlss.family=BEZI will give different results from family=BEZI
+        #fitsum<-try(summary(gamlss::gamlss(as.formula(paste(taxname[i],paste(c(comvar,adjustvar),collapse="+"),sep="~")), gamlss.family = BEZI, data = testdat, trace = FALSE),save=TRUE))
       }
       if (class(fitsum) == "try-error") {
         cat("Error in model fit, NA introduced.\n")
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is an initial version of the package *metamicrobiomeR*.
 To start, please download and view the steps/examples/workflow in the tutorial file "metamicrobiomeR_Supplementary_file". 
 
 The *metamicrobiomeR* package implements Generalized Additive Model for Location, Scale and Shape (GAMLSS) 
-    with zero inflated beta (BEZI) family for analysis of microbiome relative abundance data and 
+    with zero inflated beta (BEZI) family for analysis of microbiome relative abundance data (with various         options for data transformation/normalization to address compositional effects) and 
     random effect meta-analysis models for meta-analysis pooling estimates across microbiome studies.
     Random Forest model to predict microbiome age based on relative abundances of  
     shared bacterial genera with the Bangladesh data (Subramanian et al 2014), 
